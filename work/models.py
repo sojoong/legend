d7b8e7670dcde7332e863af1dff0c5b8ee35585c
xme1226 <xme1226@gmail.com>
2015-04-28 22:02:34 +0900

    update work models : add Banquet, Restaurant Reservation and Hall models

--- work/models.py
+++ work/models.py
@@ -1,17 +1,64 @@
 from django.db import models
 
+
 class Room(models.Model):
     type = models.CharField(max_length=30) # for example, deluxe, double, single etc..
     price = models.IntegerField(max_length=30)
+    space = models.IntegerField(max_length=10)
+    persons = models.IntegerField(max_length=10)
+    numberOfRoom = models.IntegerField(max_length=10)
 
-class Reservation(models.Model):
+
+class Hall(models.Model):
+    type = models.CharField(max_length=30)
+    persons = models.IntegerField(max_length=10)
+
+
+class RoomReservation(models.Model):
     reserveID = models.AutoField(primary_key=True)
+
+    # Personal Data
     name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=32)
+    email = models.EmailField(max_length=32)
+
+    # Stay Data
     room = models.ForeignKey('Room')
-    checkInDate = models.CharField(max_length=32)
-    checkOutDate = models.CharField(max_length=32)
-    phoneNumber = models.CharField(max_length=32)
+    checkInDate = models.DateField()
+    checkOutDate = models.DateField()
     numberOfPeople = models.IntegerField()
+    payment = models.CharField(max_length=30)
+    request = models.TextField()
+
+
+class BanquetReservation(models.Model):
+    reserveID = models.AutoField(primary_key=True)
+
+    # Personal Data
+    name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=32)
+    email = models.EmailField(max_length=32)
+
+    # Stay Data
+    hall = models.ForeignKey('Hall')
+    reservationDateTime = models.DateTimeField()
+    numberOfPeople = models.IntegerField()
+    request = models.TextField()
+
+
+class RestaurantReservation(models.Model):
+    reserveID = models.AutoField(primary_key=True)
+
+    # Personal Data
+    name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=32)
+    email = models.EmailField(max_length=32)
+
+    # Stay Data
+    reservationDateTime = models.DateTimeField()
+    numberOfPeople = models.IntegerField()
+    request = models.TextField()
+
 
 class Article(models.Model):
     articleID = models.AutuField(primary_key=True)
@@ -19,11 +66,10 @@ class Article(models.Model):
     content = models.TextField()
     image = models.ImageField()
 
+
 class Content(models.Model):
     name = models.CharField(max_length=30)
-    email = models.emailField()
+    email = models.EmailField(max_length=32)
     content = models.TextField()
     article = models.ForeignKey('Article')
 
-
-# Create your models here.
