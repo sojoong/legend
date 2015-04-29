db2f32ac1a75c7f08faef7fbf258fa2c83c81020
xme1226 <xme1226@gmail.com>
2015-04-29 21:57:43 +0900

    update views : make form and if completed, data is saved

--- legend/views.py
+++ legend/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 
+# forms
+from work.forms import RoomReservationForm
+
+# models
+from work.models import RoomReservation
+
+
 def index(request):
     return render(request, 'Sunshine/html/index.html')
 
 def booking(request):
-    return render(request, 'Sunshine/html/booking.html')
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = RoomReservationForm(request.POST)
+        # check whether it's valid:
+
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = RoomReservationForm()
+
+    return render(request, 'Sunshine/html/booking.html', {'reservation_form': form})
+
 
 def contact(request):
     return render(request, 'Sunshine/html/contact.html')
@@ -18,9 +36,6 @@ def gallery(request):
 def news(request):
     return render(request, 'Sunshine/html/news.html')
 
-def write(request):
-    return render(request, 'Sunshine/html/news_write.html')
-
 def room_details(request):
     return render(request, 'Sunshine/html/room-details.html')
 
@@ -29,3 +44,34 @@ def room_list(request):
 
 def single_news(request):
     return render(request, 'Sunshine/html/single-news.html')
+
+def complete(request):
+    if request.method == "POST":
+        form = RoomReservationForm(request.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            phone = form.cleaned_data['phone']
+            email = form.cleaned_data['email']
+            room = form.cleaned_data['room']
+            checkInDate = form.cleaned_data['checkInDate']
+            checkOutDate = form.cleaned_data['checkOutDate']
+            numberOfPeople = form.cleaned_data['numberOfPeople']
+            payment = form.cleaned_data['payment']
+            request = form.cleaned_data['request']
+
+            reservation = RoomReservation(name=name, phone=phone, email=email, room=room,
+                                          checkInDate=checkInDate, checkOutDate=checkOutDate,
+                                          numberOfPeople=numberOfPeople, payment=payment,
+                                          request=request)
+
+            reservation.save()
+
+            return render(request, "Sunshine/html/room-list.html", {'reservation_form' : form})
+
+    return render(request, "Sunshine/html/single-news.html", {'reservation_form' : form})
+
+
+
+def write(request):
+    return render(request, 'Sunshine/html/news_write.html')
